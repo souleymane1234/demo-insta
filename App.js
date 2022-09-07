6808b477b554380feb20be902604e245fb749f8c
souleymane1234 <konatesouleymane42@gmail.com>
2022-09-07 20:26:41 +0200

    my commit

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState, useContext } from "react";
+import Router from "./src/navigator/Router";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from "./src/screens/home";
+import Login from "./src/screens/login";
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+            <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name={"Login"}
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name={"Home"}
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/login/index.js
@@ -0,0 +1,145 @@
+import React, { useState } from 'react';
+import { ImageBackground, View, Text, StyleSheet, TouchableOpacity, TextInput, Platform } from 'react-native';
+
+
+const Login = ({navigation}) => {
+
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [data, setData] = useState({});
+
+
+    const onSubmitHandler = () => {
+      var myHeaders = new Headers();
+      myHeaders.append("Cache-Control", "no-cache");
+      myHeaders.append("Accept", "*/*");
+      myHeaders.append("Accept-Encoding", "gzip, deflate");
+      myHeaders.append("Connection", "keep-alive");
+      myHeaders.append("Content-Type", "application/json");
+
+      var raw = JSON.stringify({
+        "name": name,
+        "password": password
+      });
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch("https://appforlogin.herokuapp.com/api/login", requestOptions)
+      .then(response => response.json())
+      .then((json) => {
+        setData(json)
+        console.log('data^^^^^^^^^^^',data)
+        navigation.navigate("Home", {
+                data
+            })
+      })
+      .catch((error) => console.error(error))
+    
+    };
+
+    const getMessage = () => {
+        navigation.navigate("Home")
+    }
+
+    return (
+        <ImageBackground source={require('../../assests/img.jpg')} style={styles.image}>
+            <View style={styles.card}>
+                <Text style={styles.heading}>Connect API</Text>
+                <View style={styles.form}>
+                    <View style={styles.inputs}>
+                        <TextInput style={styles.input} placeholder="Username" autoCapitalize="none" onChangeText={setName}></TextInput>
+                        <TextInput style={styles.input} placeholder="pass" autoCapitalize="none" onChangeText={setPassword}></TextInput>
+                        <TouchableOpacity style={styles.button} onPress={() => onSubmitHandler()}>
+                            <Text style={styles.buttonText}>Done</Text>
+                        </TouchableOpacity>
+                    </View>    
+                </View>
+            </View>
+        </ImageBackground>
+    );
+};
+
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+    },  
+    card: {
+        flex: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        width: '80%',
+        marginTop: '40%',
+        borderRadius: 20,
+        maxHeight: 380,
+        paddingBottom: '30%',
+    },
+    heading: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginLeft: '10%',
+        marginTop: '5%',
+        marginBottom: '30%',
+        color: 'black',
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingBottom: '5%',
+    },
+    inputs: {
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: '10%',
+    },  
+    input: {
+        width: '80%',
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+        paddingTop: 10,
+        fontSize: 16, 
+        minHeight: 40,
+    },
+    button: {
+        width: '80%',
+        backgroundColor: 'black',
+        height: 40,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '400'
+    },
+    buttonAlt: {
+        width: '80%',
+        borderWidth: 1,
+        height: 40,
+        borderRadius: 50,
+        borderColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 5,
+    },
+    buttonAltText: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: '400',
+    },
+    message: {
+        fontSize: 16,
+        marginVertical: '5%',
+    },
+});
+
+export default Login;
